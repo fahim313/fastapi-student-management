ebb47e089696369f0313c5cee6286c6cc2f613d1
Fahim sahriar <fahimsahriar2002@gmail.com>
2026-08-11 15:34:14 +0600

    add Pydantic data validation

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import json
 from fastapi import FastAPI,Path,HTTPException,Query,Body 
-
+from pydantic import BaseModel,Field
+from typing import Annotated 
 app = FastAPI()
+
+class Student(BaseModel):
+    id: Annotated[str, Field(..., description="Unique ID of the student", examples=["S012"])]
+    name: Annotated[str, Field(..., description="Full name of the student", examples=["Nayeem Hasan"])]
+    age: Annotated[int, Field(..., gt=5, lt=19, description="Student age", examples=[15])]
+    student_class: Annotated[int, Field(..., gt=0, lt=13, description="Student class", examples=[9])]
+    roll: Annotated[int, Field(..., gt=0, description="Roll number", examples=[5])]
+    Math_marks: Annotated[int, Field(..., gt=-1, lt=101, description="Math marks", examples=[80])]
+    English_marks: Annotated[int, Field(..., gt=-1, lt=101, description="English marks", examples=[85])]
+    Science_marks: Annotated[int, Field(..., gt=-1, lt=101, description="Science marks", examples=[87])]
+    phone: Annotated[
+        str,
+        Field(
+            ...,
+            pattern=r"^01[3-9]\d{8}$",
+            description="Bangladesh mobile phone number",
+            examples=["01710000012"]
+        )
+    ]
+    
+
 
 #load data
 
@@ -46,8 +68,8 @@ def get_student(student_id:str = Path(...,description="The ID of the student to 
 
 @app.get("/sort_students")
 def view_sorted_students(sorted_by:str = Query(...,
-description="sort on the basis of class,age, roll,marks"),order:str = Query("asc",description="Choose order:asc or desc")):
-    valid_fields = ["age","class","rool","Math marks","English marks","Science marks"]
+description="sort on the basis of student_class,age, roll,marks"),order:str = Query("asc",description="Choose order:asc or desc")):
+    valid_fields = ["age","student_class","rool","Math_marks","English_marks","Science_marks"]
     
     if sorted_by not in valid_fields:
         raise HTTPException(status_code=404, detail=f"Invalid field. select from{valid_fields}")
@@ -69,25 +91,21 @@ description="sort on the basis of class,age, roll,marks"),order:str = Query("asc
 # create student 
 
 @app.post("/create")
-def create_student(student: dict = Body(...)):
+def create_student(student: Student):
 
     data = load_data()
 
-    student_id = student["id"]
-
-    if student_id in data:
+    if student.id in data:
         raise HTTPException(
             status_code=400,
             detail="Student already exists"
         )
 
-    del student["id"]
-
-    data[student_id] = student
+    data[student.id] = student.model_dump(exclude=["id"])
 
     save_data(data)
 
     return {
         "message": "Student created successfully",
-        "student_id": student_id
+        "student_id": student.id
     }
